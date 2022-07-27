99ba2401e7587f013ed2a6887dce3717570cb7f7
makoma mabilo <makoma44@gmail.com>
2022-07-27 16:08:29 +0800

    added html structure and started card stying

---
+++ script.js
@@ -0,0 +1,11 @@
+// const trivia = document.getElementById('app')
+// const scoreDisplay = document.getElementById('score')
+
+
+// function addCard() {
+//   const card = document.createElement('div')
+//   card.classList.add('options-card')
+//   card.innerHTML = 'insert questions and answer'
+//   trivia.append(card)
+// }
+// addCard()
